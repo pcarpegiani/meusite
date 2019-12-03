a4f53e8fda9a0499c23397422c5c787da17a02e7
Paulo Carpegiani <56129581+pcarpegiani@users.noreply.github.com>
2019-12-03 20:48:31 -0300

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c";
+var enderecoContrato = "0x2A929beC871E0c01F60876Cd3F8814A87a585f34";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
